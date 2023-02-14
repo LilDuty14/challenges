a6ee6bbf7481bf176d1128c4dc770858d5202d3d
LilDuty14 <csgocarlos1@gmail.com>
2023-02-13 20:57:25 -0400

    listo creo no se como funciona esto:(

--- 1/styles/scritp.js
+++ 1/styles/scritp.js
@@ -1,48 +1,87 @@
-const textarea = document.querySelector(".textarea");
-const mensaje = document.querySelector("mensaje");
+var botonEncriptar = document.querySelector(".btn-encriptar");
+var botonDesencriptar = document.querySelector(".btn-desencriptar");
+var munheco = document.querySelector(".contenedor-de-munheco");
+var h3 = document.querySelector(".contenedor-de-h3");
+var parrafo = document.querySelector(".contenedor-de-parrafo");
+var resultado = document.querySelector(".texto-resultado");
+var copiar = document.querySelector(".btn-copiar");
+const area = document.querySelector(".area")
 
-//La letra "e" es convertida para "enter"
-//La letra "i" es convertida para "imes"
-//La letra "a" es convertida para "ai"
-//La letra "o" es convertida para "ober"
-//La letra "u" es convertida para "ufat"
+botonEncriptar.onclick = encriptar;
+botonDesencriptar.onclick = desencriptar;
+// copiar.onclick = recuperarTexto;
+copiar.addEventListener("click", copiarTexto);
 
-function btnencriptar(){
-    const textoencriptado = encriptar(textArea.value)
-    mensaje.value = textoencriptado
-    textarea.value = "";
-    mensaje.style.backgroundImage = "none"
+
+function encriptar() {
+  ocultarAdelante();
+  var area = recuperarTexto();
+  resultado.textContent = encriptarTexto(area);
 }
 
-function encriptar(stringEncriptada){
-    let matrizcodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    stringEncriptada = stringEncriptada.toLowerCase()
 
-        for(let i= 0; i < matrizcodigo.length; i++){
-            if(stringEncriptada.includes(matrizcodigo[i][0])){
-                stringEncriptada = stringEncriptada.replaceAll(matrizcodigo[i][0], matrizcodigo[i][1])
-
-            }
-        }
-        return stringEncriptada
+function desencriptar() {
+  ocultarAdelante();
+  var area = recuperarTexto();
+  resultado.textContent = desencriptarTexto(area);
+}
+function ocultarAdelante() {
+  munheco.classList.add("ocultar");
+  h3.classList.add("ocultar");
+  parrafo.classList.add("ocultar");
+}
+function encriptarTexto(mensaje) {
+  var texto = mensaje;
+  var textoFinal = "";
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i] == "a") {
+      textoFinal = textoFinal + "ai";
+    } else if (texto[i] == "e") {
+      textoFinal = textoFinal + "enter";
+    } else if (texto[i] == "i") {
+      textoFinal = textoFinal + "imes";
+    } else if (texto[i] == "o") {
+      textoFinal = textoFinal + "ober";
+    } else if (texto[i] == "u") {
+      textoFinal = textoFinal + "ufat";
+    } else {
+      textoFinal = textoFinal + texto[i];
+    }
+  }
+  return textoFinal;
+}
+function desencriptarTexto(mensaje) {
+  var texto = mensaje;
+  var textoFinal = "";
+  for (var i = 0; i < texto.length; i++) {
+    if (texto[i] == "a") {
+      textoFinal = textoFinal + "a";
+      i = i + 1;
+    } else if (texto[i] == "e") {
+      textoFinal = textoFinal + "e";
+      i = i + 4;
+    } else if (texto[i] == "i") {
+      textoFinal = textoFinal + "i";
+      i = i + 3;
+    } else if (texto[i] == "o") {
+      textoFinal = textoFinal + "o";
+      i = i + 3;
+    } else if (texto[i] == "u") {
+      textoFinal = textoFinal + "u";
+      i = i + 3;
+    } else {
+      textoFinal = textoFinal + texto[i];
+    }
+  }
+  return textoFinal;
 }
 
-function btndesencriptar(){
-    const textoencriptado = desencriptar(textArea.value)
-    mensaje.value = textoencriptado
-    textarea.value = "";
+function recuperarTexto() {
+  return document.querySelector(".area").value;
 }
 
-function desencriptar(stringDesencriptada){
-    let matrizcodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]
-    stringDesencriptada = stringDesencriptada.toLowerCase()
-
-        for(let i= 0; i < matrizcodigo.length; i++){
-            if(stringDesencriptada.includes(matrizcodigo[i][1])){
-                stringDesencriptada = stringDesencriptada.replaceAll(matrizcodigo[i][1], matrizcodigo[i][0])
-
-            }
-        }
-        return stringDesencriptada
+function copiarTexto() {
+  var textoResultado = document.querySelector(".texto-resultado");
+  navigator.clipboard.writeText(textoResultado.value)
+  area.value = textoResultado.value
 }
-
